5d7be18bf39087f6f245a33a7b9087319b4bc5cc
Chandnaik <chandnaik53@gmail.com>
2023-09-06 15:01:37 +0530

    Arithmetic operation

---
+++ first.py
@@ -0,0 +1,14 @@
+
+# Arithmetic operation
+a = 56
+b = 23
+add = a + b
+sub = a - b
+multiply = a * b
+division =  a/b
+
+print('Sum of {} and {} is {}'.format(a,b,add))
+print('Diffrence between {} and {} is {}'.format(a,b,sub))
+print('Product of {} and {} is {}'.format(a,b,multiply))
+print('Division of {} and {} is {}'.format(a,b,division))
+
