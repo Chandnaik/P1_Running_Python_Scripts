15511cc8f072f9576c94bcf3d269635c61400bd5
Chandnaik <chandnaik53@gmail.com>
2023-09-06 15:54:08 +0530

    Condition

--- first.py
+++ first.py
@@ -20,3 +20,40 @@ print(type(float(a)))
 print((bool(a)))
 print(a)
 print(type(a))
+
+
+
+
+
+
+# if else loop
+if a > b:
+    print('{} is greater'.format(a))
+elif a == b:
+    print('{} is equal to {}'.format(a,b))
+else:
+    print('{} is smaller'.format(a))
+print("out from if else condition")
+
+
+
+
+
+#list
+flower = ["rose","lotus","champa","mogra"]
+for f in flower:
+    print(f)
+print("the flowers name as been printed")
+
+
+#numbers
+number = [20,476,34,872,67]
+print("number in list")
+for n in number:
+    print(n)
+print("number greater than 50 are")
+for x in number:
+    if x > 50:
+        print(x)
+
+
